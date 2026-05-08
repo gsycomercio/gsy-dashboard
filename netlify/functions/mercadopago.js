e59d0d084888e8a61a9ffeb3a2117822330d9d66
gsycomercio <gsycomercio@gmail.com>
2026-05-08 18:54:12 -0300

    Update mercadopago.js

--- netlify/functions/mercadopago.js
+++ netlify/functions/mercadopago.js
@@ -3,9 +3,9 @@ exports.handler = async function(event) {
   const endpoint = event.queryStringParameters && event.queryStringParameters.endpoint;
 
   const endpoints = {
-    balance:   'https://api.mercadopago.com/v1/account/balance',
-    releases:  'https://api.mercadopago.com/v1/account/release/search?offset=0&limit=50',
-    payments:  'https://api.mercadopago.com/v1/payments/search?sort=date_created&criteria=desc&range=date_created&begin_date=NOW-30DAYS&end_date=NOW&limit=50',
+    balance:  'https://api.mercadopago.com/v1/account/balance',
+    releases: 'https://api.mercadopago.com/v1/account/release/search?offset=0&limit=100',
+    payments: 'https://api.mercadopago.com/v1/payments/search?sort=date_created&criteria=desc&range=date_created&begin_date=NOW-30DAYS&end_date=NOW&limit=50',
   };
 
   if (!endpoint || !endpoints[endpoint]) {
@@ -14,7 +14,10 @@ exports.handler = async function(event) {
 
   try {
     const response = await fetch(endpoints[endpoint], {
-      headers: { 'Authorization': 'Bearer ' + token }
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json',
+      }
     });
     const data = await response.json();
     return {
@@ -28,6 +31,7 @@ exports.handler = async function(event) {
   } catch (err) {
     return {
       statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
       body: JSON.stringify({ error: err.message }),
     };
   }
